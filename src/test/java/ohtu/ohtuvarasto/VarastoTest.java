5a9e72898077c357f6973a9f611224f20e0e459f
Eeva-Maria Laiho <eeva-maria@laiho.net>
2017-11-03 20:15:23 +0200

    added tests

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -65,4 +65,85 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+
+    @Test
+    public void ottaminenTesti1() {
+
+    	// varastossa pitäisi olla tilaa 10 - 12 = -2
+        assertEquals(0.0, varasto.otaVarastosta(-10), vertailuTarkkuus);
+    }
+    
+
+    @Test
+    public void lisaaTesti1() {
+
+    	double saldo = varasto.getSaldo();
+    	varasto.lisaaVarastoon(-10);
+    	
+    	// varastossa pitäisi olla tilaa 10 - 12 = -2
+        assertEquals(saldo, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTesti2() {
+
+    	
+    	double value = varasto.getSaldo() + varasto.paljonkoMahtuu();
+    	varasto.lisaaVarastoon(varasto.paljonkoMahtuu() + 1);
+    	
+    	// varastossa pitäisi olla tilaa 10 - 12 = -2
+        assertEquals(value, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    //return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
+    @Test
+    public void toStringTesti1() {
+    	
+    	varasto.lisaaVarastoon(5);
+        assertEquals("saldo = 5.0, vielä tilaa 5.0", varasto.toString());
+    }
+    
+    @Test
+    public void konstruktoriTesti1() {
+    	
+    	//public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+    	Varasto tmp = new Varasto(-1,5);
+    	
+        assertEquals(0.0,tmp.getTilavuus(),vertailuTarkkuus);
+    }   
+    
+    @Test
+    public void konstruktoriTesti2() {
+    	
+    	//public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+    	Varasto tmp = new Varasto(5,-1);
+    	
+        assertEquals(0.0,tmp.getSaldo(),vertailuTarkkuus);
+    } 
+    
+
+    @Test
+    public void konstruktoriTesti3() {
+    	
+    	//public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+    	Varasto tmp = new Varasto(5,2);
+    	
+        assertEquals(5.0,tmp.getTilavuus(),vertailuTarkkuus);
+        assertEquals(2.0,tmp.getSaldo(),vertailuTarkkuus);
+       
+    } 
+
+
+    @Test
+    public void konstruktoriTesti4() {
+    	
+    	//public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+    	Varasto tmp = new Varasto(5,2);
+    	
+        assertEquals(5.0,tmp.getTilavuus(),vertailuTarkkuus);
+        assertEquals(2.0,tmp.getSaldo(),vertailuTarkkuus);
+       
+    } 
+    
+    
 }
